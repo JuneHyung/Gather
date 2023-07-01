863d66c9d386c8ed76f71d2f40affb683707f71d
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-07-02 01:33:30 +0900

    feat : chart xAxis, yAxis설정및 그리기

--- gather/src/pages/canvas/CanvasChart.jsx
+++ gather/src/pages/canvas/CanvasChart.jsx
@@ -17,21 +17,29 @@ const chartData = {
 };
 
 const CanvasChart = () => {
-  const draw = () => {
-    const topBox = document.getElementById("top-box");
-    const canvas = document.getElementById("custom-chart");
-    // 캔버스 크기 설정
-    // canvas.width = topBox.clientWidth; // 캔버스 너비
-    // canvas.height = topBox.clientHeight; // 캔버스 높이
-    canvas.width = 1000; // 캔버스 너비
-    canvas.height = 500; // 캔버스 높이
+  const containerRef = useRef(null);
+  const canvasRef = useRef(null);
 
+  const draw = () => {
+    const container = containerRef.current;
+    const canvas = canvasRef.current;
+    canvas.width = container.clientWidth; // 캔버스 너비
+    canvas.height = container.clientHeight; // 캔버스 높이
+
+    const gap = 50;
     if (canvas.getContext) {
       const ctx = canvas.getContext("2d");
-      const xAxisLabels = chartData.labels;
+      // const xAxisLabels = chartData.labels;
       const datasets = chartData.datasets;
+
+      const makeXAxis = () => {
+        const result = chartData.labels;
+        return result;
+      };
+
       // 최소 최대수를 가지고 yAxis에 표시될 배열 만들기
       const makeYAxis = () => {
+        // const datasets = chartData.datasets;
         const minD = Math.min(...datasets[0].data);
         const maxD = Math.max(...datasets[0].data);
         const step = 5;
@@ -40,86 +48,117 @@ const CanvasChart = () => {
         const len = Math.ceil((end - start) / 5) + 1;
 
         const arr = Array.from({ length: len }, (_, i) => start + i * step);
+        canvas.height = len * gap;
         return arr;
       };
-      // 초기 xAxis draw
-      const drawXAxis = () => {
-        // const yAxisMaxValue  = Math.max(...data);
-        const xAxisStartX = 50; // x-axis 시작 위치
-        const xAxisEndX = canvas.width - 50; // x-axis 끝 위치
-        const xAxisY = canvas.height - 50; // x-axis y 위치
 
+      const xAxis = makeXAxis();
+      const yAxis = makeYAxis();
+      let xGap = 50;
+      let yGap = 50;
+
+      const drawXLines = (ctx, startX, endX, Y) => {
         ctx.beginPath();
-        ctx.moveTo(xAxisStartX, xAxisY);
-        ctx.lineTo(xAxisEndX, xAxisY);
-        ctx.strokeStyle = "#ffffff";
-        ctx.fillStyle = "#ffffff";
+        ctx.moveTo(startX, Y);
+        ctx.lineTo(endX, Y);
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "#FFFFFF";
+        // ctx.fillStyle = "#FFFFFF";
         ctx.stroke();
+      };
 
-        // x축 라벨 그리기
-        const labelSpacing = (xAxisEndX - xAxisStartX) / (xAxisLabels.length - 1);
-        xAxisLabels.forEach((label, index) => {
-          const x = xAxisStartX + labelSpacing * index;
-          ctx.fillText(label, x - 20, xAxisY + 20);
+      const drawYLines = (ctx, startY, endY, X) => {
+        ctx.beginPath();
+        ctx.moveTo(X, startY);
+        ctx.lineTo(X, endY);
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "#FFFFFF";
+        // ctx.fillStyle = "#FFFFFF";
+        ctx.stroke();
+      };
+
+      const drawXLabels = (ctx, startX, endX, Y) => {
+        xGap = (endX - startX) / (xAxis.length - 1);
+        xAxis.forEach((label, index) => {
+          const x = startX + xGap * index;
+          ctx.fillStyle = "#FFFFFF";
+          ctx.font="0.8rem malgaun gothic";
+          const fontLen = label.length *4
+          ctx.fillText(label, x - fontLen, Y + 20);
         });
       };
-      // 초기 yAxis draw
-      const drawYAxis = () => {
-        const yAxisStartY = 50; // y-axis 시작 위치
-        const yAxisEndY = canvas.height - 50; // y-axis 끝 위치
-        const yAxisX = 50; // y-axis x 위치
 
-        ctx.beginPath();
-        ctx.moveTo(yAxisX, yAxisStartY);
-        ctx.lineTo(yAxisX, yAxisEndY);
-        ctx.strokeStyle = "#ffffff";
-        ctx.lineJoin = "round";
-        ctx.stroke();
-
-        // y축 라벨 그리기
-        const yAxis = makeYAxis();
-
-        const labelSpacing = (yAxisEndY - yAxisStartY) / (yAxis.length - 1);
+      const drawYLabels = (ctx, startY, endY, X) => {
+        yGap = (endY - startY) / (yAxis.length - 1);
         yAxis.forEach((value, index) => {
-          const y = yAxisEndY - labelSpacing * index;
-          ctx.fillText(value.toString(), yAxisX - 30, y);
+          const y = endY - yGap * index;
+          ctx.fillStyle = "#FFFFFF";
+          ctx.font="0.8rem malgaun gothic";
+          ctx.fillText(value.toString(), X - 30, y);
         });
       };
 
-      drawXAxis();
-      drawYAxis();
+      const drawAxis = () => {
+        const xAxisStartX = 50; // x-axis 시작
+        const xAxisEndX = canvas.width - gap; // x-axis 끝
+        let xAxisY = canvas.height - yGap; // 시작점Y
+        const xlen = yAxis.length; // 그릴 LINE 갯수
+        drawXLabels(ctx, xAxisStartX, xAxisEndX, xAxisY);
 
-      // X축 정보
-      const xPadding = 50; // X축 여백
-      const xStart = xPadding; // X축 시작 위치
-      const xEnd = canvas.width - xPadding; // X축 끝 위치
-      const xRange = xEnd - xStart; // X축 범위
-      const xStep = xRange / (xAxisLabels.length - 1); // X축 간격
+        const yAxisStartY = 50; // y-axis 시작
+        const yAxisEndY = canvas.height - gap; // y-axis 끝
+        let yAxisX = 50; // y-axis x 위치
+        const ylen = xAxis.length;
+        drawYLabels(ctx, yAxisStartY, yAxisEndY, yAxisX);
 
-      // Y축 정보
-      const yPadding = 50; // Y축 여백
-      const yStart = canvas.height - yPadding; // Y축 시작 위치
-      const yEnd = yPadding; // Y축 끝 위치
-      const yRange = yStart - yEnd; // Y축 범위
-      const yStep = yRange / (Math.max(...datasets[0].data) - Math.min(...datasets[0].data)); // Y축 간격
+        for (let i = 0; i < xlen; i++) {
+          drawXLines(ctx, xAxisStartX, xAxisEndX, xAxisY);
+          xAxisY -= yGap;
+        }
+        for (let i = 0; i < ylen; i++) {
+          drawYLines(ctx, yAxisStartY, yAxisEndY, yAxisX);
+          yAxisX += xGap;
+        }
+      };
 
-      ctx.beginPath();
-      ctx.moveTo(xStart, yStart);
-      for (let i = 0; i < xAxisLabels.length; i++) {
-        let x = xStart + i * xStep; // X 좌표 계산
-        let y = yStart - (datasets[0].data[i] - Math.min(...datasets[0].data)) * yStep; // Y 좌표 계산
-        ctx.lineTo(x, y); // 점에 연결
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = "#ffffff";
-        ctx.fillText(datasets[0].data[i], x - 5, y - 10); // 데이터 값 텍스트 표시
-        ctx.lineJoin = "round";
-        ctx.stroke(); // 선 그리기
+      drawAxis();
 
+      const drawDataLine = () => {
+        // x축 정보
+        const xStart = 50; // X축 시작 위치
+        // const xEnd = canvas.width - gap; // X축 끝 위치
+        // const xRange = xEnd - xStart; // X축 범위
+        // const xStep = xRange / (xAxis.length - 1); // X축 간격
+
+        // y축정보
+        const yStart = canvas.height - gap; // Y축 시작 위치
+        const yEnd = 50; // Y축 끝 위치
+        const yRange = yStart - yEnd; // Y축 범위
+        const yStep = yRange / (Math.max(...yAxis) - Math.min(...yAxis)); // Y축 간격
+
+        ctx.moveTo(xStart, yStart);
         ctx.beginPath();
-        ctx.arc(x, y, 4, 0, Math.PI * 2);
-        ctx.fillStyle = "#ffffff";
-        ctx.fill();
-      }
+        for (let i = 0; i < xAxis.length; i++) {
+          let x = xStart + i * xGap; // X 좌표 계산
+          let y = yStart - (datasets[0].data[i] - Math.min(...yAxis)) * yStep; // Y 좌표 계산
+          ctx.lineTo(x, y); // 점에 연결
+          ctx.lineWidth = 3;
+          ctx.strokeStyle = "rgb(255, 99, 132)";
+          ctx.fillStyle = "#fff";
+          ctx.font="0.8rem malgaun gothic"
+          const fontLen = datasets[0].data[i].toString().length*4;
+          ctx.fillText(datasets[0].data[i], x-fontLen, y - 10); // 데이터 값 텍스트 표시
+          ctx.lineJoin = "round";
+          ctx.stroke(); // 선 그리기
+
+          ctx.beginPath();
+          ctx.arc(x, y, 4, 0, Math.PI * 2);
+          ctx.fillStyle = "rgb(255, 99, 132)";
+          ctx.fill();
+        }
+      };
+
+      drawDataLine();
     }
   };
   useEffect(() => {
@@ -129,9 +168,9 @@ const CanvasChart = () => {
   return (
     <div className="canvas-chart-wrap-box">
       <div className="canvas-chart-content-wrap">
-        <div id="top-box" className="top-box">
+        <div ref={containerRef} id="container-box" className="top-box">
           <h2>Custom Canvas</h2>
-          <canvas id="custom-chart"></canvas>
+          <canvas ref={canvasRef} id="custom-chart"></canvas>
         </div>
         <hr />
         <div className="bottom-box">
